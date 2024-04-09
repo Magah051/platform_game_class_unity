ca78f2aebe2ce1e8aa10079682837f1a12fbdd0a
José Magalhães <joses.magalhaes@hotmail.com>
2024-04-09 20:42:14 -0300

    :construction: Animação do player walk

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,30 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f; 
     private Rigidbody2D rb;
     private float moveInput;
+    private Animator animator;
+    private bool isRunning = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>(); 
+        animator = GetComponent<Animator>(); 
+        animator.SetBool("isRunning", false); 
     }
 
     void Update()
     {
         moveInput = Input.GetAxisRaw("Horizontal");
+
+        if (moveInput != 0)
+        {
+            isRunning = true;
+        }
+        else
+        {
+            isRunning = false;
+        }
+
+        animator.SetBool("isRunning", isRunning);
     }
 
     void FixedUpdate()
@@ -24,7 +39,6 @@ public class PlayerController : MonoBehaviour
         {
             transform.localScale = new Vector3(1f, 1f, 1f); 
         }
-
         else if (moveInput < 0)
         {
             transform.localScale = new Vector3(-1f, 1f, 1f);
