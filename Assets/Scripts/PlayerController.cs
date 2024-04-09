63bc427da37543acff3936885d8a34108e4e2c4d
José Magalhães <joses.magalhaes@hotmail.com>
2024-04-09 20:43:12 -0300

    :construction: Implementação do pulo do Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f; 
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
     private Rigidbody2D rb;
     private float moveInput;
     private Animator animator;
@@ -10,9 +11,9 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>(); 
-        animator = GetComponent<Animator>(); 
-        animator.SetBool("isRunning", false); 
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        animator.SetBool("isRunning", false);
     }
 
     void Update()
@@ -29,6 +30,11 @@ public class PlayerController : MonoBehaviour
         }
 
         animator.SetBool("isRunning", isRunning);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
     }
 
     void FixedUpdate()
@@ -37,11 +43,21 @@ public class PlayerController : MonoBehaviour
 
         if (moveInput > 0)
         {
-            transform.localScale = new Vector3(1f, 1f, 1f); 
+            transform.localScale = new Vector3(1f, 1f, 1f);
         }
+
         else if (moveInput < 0)
         {
             transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+    }
+
+    void Jump()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.1f);
+        if (hit.collider != null)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
     }
 }
